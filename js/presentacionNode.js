a78934d90ca61d78627539514c5468f27ead9874
tucopablo <lauson@alu.edu.gva.es>
2025-10-13 12:01:24 +0200

    Ajout du fichier presentacionNode.js - Laurent Sontag

---
+++ js/presentacionNode.js
@@ -0,0 +1,5 @@
+console.log("Laurent Sontag");
+//Conseguido
+// para mi errores a no repetir
+    // -No abrir Node fuera pero dentro de VScode
+    // -en commendo bien poner node antes del nomdre del fichero (no habia entendido que habia q ponerlo)
---
+++ para mi Tareas 12_10_2025/JSexemples.js
@@ -0,0 +1,3 @@
+let year = prompt('¿En que año fué publicada la especificación ECMAScript-2015?', '');
+
+if (year == 2015) alert( '¡Estás en lo cierto!' );
