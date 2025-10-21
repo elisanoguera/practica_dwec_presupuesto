f391759339a7bdd832fcdb54df7e080b1ad81e12
AOP2526 <crisor1234@gmail.com>
2025-10-21 10:47:39 +0200

    Práctica 1: Se añade variable global presupuesto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,45 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
+let presupuesto = 0;
+
 // TODO: Variable global
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(nuevoPresupuesto) {
+    if (typeof nuevoPresupuesto === 'number' && nuevoPresupuesto >= 0) {
+        presupuesto = nuevoPresupuesto;
+        return presupuesto;
+    } else {
+        console.error('Error: El presupuesto debe ser un número no negativo');
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+
+    if (typeof valor !== 'number' || valor < 0) {
+        valor = 0;
+    }
+    
+    this.descripcion = descripcion || '';
+    this.valor = valor;
+    
+    this.mostrarGasto = function() {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    };
+    
+    this.actualizarDescripcion = function(nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion || '';
+    };
+    
+    this.actualizarValor = function(nuevoValor) {
+        if (typeof nuevoValor === 'number' && nuevoValor >= 0) {
+            this.valor = nuevoValor;
+        }
+    };
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
