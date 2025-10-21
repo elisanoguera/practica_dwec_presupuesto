249be7bbe0b552c7e3e064f2876ed2e3f4d1462c
OscarDGimenezMunzon <oscgimmun@alu.edu.gva.es>
2025-10-21 21:04:23 +0200

    Creada función constructora CrearGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,7 +4,7 @@
 let presupuesto = 0;
 
 function actualizarPresupuesto(valor) {
-    
+
     if (valor >= 0) {
         presupuesto = valor;
         return presupuesto;
@@ -21,14 +21,26 @@ function mostrarPresupuesto() {
     return presupuesto;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(valor) {
+    this.descripcion = "Descripción del gasto en formato cadena";
+    this.valor = (valor >= 0) ? valor : 0;  // En caso de que el valor introducido no sea un núḿero no negativo, se asigna el valor 0.
+    this.mostrarGasto = function () {
+        alert(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
+    };
+    this.actualizarDescripcion = function (nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    };
+    this.actualizarValor = function (nuevoValor) {
+        if (nuevoValor >= 0) {
+            this.valor = nuevoValor;
+        }
+    };
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
