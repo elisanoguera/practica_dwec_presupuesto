09d9624a6d8ae43f4ef33dd46d3304a9e243fc5f
AlvaroAltavista <alvaroaltavista@gmail.com>
2025-10-22 18:07:40 +0200

    Función actualizarPresupuesto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,10 +1,16 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
 let presupuesto = 0;
 
-function actualizarPresupuesto() {
-	// TODO
+function actualizarPresupuesto(valor) {
+	// Debe cumplir dos condiciones: que sea número y que sea mayor o igual que 0
+	// Number.isFinite(valor) comprueba si el parámetro que pasamos es un número además de otras comprobaciones como NaN e infinito.
+	if (Number.isFinite(valor) && valor >= 0) {
+		presupuesto = valor;
+		return presupuesto;
+	} else {
+		return -1;
+	}
 }
 
 function mostrarPresupuesto() {
@@ -12,9 +18,7 @@ function mostrarPresupuesto() {
 	return texto;
 }
 
-function CrearGasto() {
-	// TODO
-}
+function CrearGasto() {}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
