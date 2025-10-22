4b5d4ac77ab9f15ee1c81a87b9f16d6f3afa5069
Jens Peter Byhan Camacho <jenbyhcam@alu.edu.gva.es>
2025-10-22 18:42:18 +0200

    Realización tarea fundamentos js I

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,24 +1,40 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
+let presupuesto = 0;
 
+function actualizarPresupuesto(valor) {
 
-function actualizarPresupuesto() {
-    // TODO
+    if (valor >= 0) {
+        presupuesto = valor;
+        return presupuesto;
+    } else {
+        console.log("El presupuesto no es positivo")
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`
 }
 
-function CrearGasto() {
-    // TODO
-}
+function CrearGasto(descripcion, val) {
+    this.descripcion = descripcion;
+    this.valor = (val >=0) ? val:0;
 
+    this.mostrarGasto = function(){
+            return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`
+    }
+    this.actualizarDescripcion = function(nuevaDescripcion){
+        this.descripcion = nuevaDescripcion;
+    }
+    this.actualizarValor = function(valor){
+        this.valor = (valor >=0) ? valor: this.valor;
+    }
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
