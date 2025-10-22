e469d8670376bf73897e4b25b374594de2214559
RAquel-Lillo <raquel.lillomarco@gmail.com>
2025-10-22 19:05:53 +0200

    Implementada función CrearGasto con propiedades y métodos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,6 +1,7 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
 
 function actualizarPresupuesto() {
@@ -11,9 +12,36 @@ function mostrarPresupuesto() {
     // TODO
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    this.descripcion = String(descripcion);
+
+    // Si el tipo de dato valor es de tipo numero y es mayor o igual a 0 y no es null
+        if (typeof valor === 'number' && valor >= 0 && !isNaN(valor)) {
+            this.valor = valor;
+        } else {
+            this.valor = 0;
+        };
+    // Métodos
+    this.mostrarGasto = function() {
+        return 'Gasto correspondiente a ' + this.descripcion + ' con valor ' + this.valor + ' €';
+    }
+    // La descripcion sienpre sera una cadena 
+    this.actualizarDescripcion = function(newDescripcion) {
+        this.descripcion = String(newDescripcion);
+    }
+    // Actualiza el valor del gasto si el nuevo valor es un número válido y no es un numero negativo
+    this.actualizarValor = function(newValor) {
+        if (typeof newValor === 'number' && newValor >= 0 && !isNaN(newValor)) {
+            this.valor = newValor;
+        }   
+
 }
+
+//objeto gasto
+let gasto1 = new CrearGasto('Compra semanal', 50);
+gasto1.descripcion; // 'Compra semanal'
+gasto1.valor; // 50
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
