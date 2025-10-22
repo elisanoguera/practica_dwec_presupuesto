4582612a05a0830460d7ef4a529c1856224bf799
AlvaroAltavista <alvaroaltavista@gmail.com>
2025-10-22 17:31:41 +0200

    mostrarPresupuesto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,25 +1,22 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
-
+let presupuesto = 0;
 
 function actualizarPresupuesto() {
-    // TODO
+	// TODO
 }
 
 function mostrarPresupuesto() {
-    // TODO
+	let texto = `Tu presupuesto actual es de ${presupuesto} €`;
+	return texto;
 }
 
 function CrearGasto() {
-    // TODO
+	// TODO
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto
-}
+export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto };
