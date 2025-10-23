4681960314884d715d47b6d8be76b5e6b677074b
AlvaroAltavista <alvaroaltavista@gmail.com>
2025-10-23 19:03:01 +0200

    Actualización de mostrarGastoCompleto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -24,20 +24,48 @@ function mostrarPresupuesto() {
 	return texto;
 }
 
-function CrearGasto(descripcion, valor) {
+function listarGastos() {
+	return gastos;
+}
+
+function anyadirGasto() {}
+
+function borrarGasto() {}
+
+function calcularTotalGastos() {}
+
+function calcularBalance() {}
+
+// Función Constructora del objeto Gasto
+// Pasamos los parámetros a partir del tercero como un array a las etiquetas
+function CrearGasto(descripcion, valor, ...etiquetas) {
 	// PROPIEDADES ----------------------------------
 	this.descripcion = descripcion;
 
-	// Debemos validar que es un número no negativo
+	// Valor - Debemos validar que es un número no negativo
 	if (Number.isFinite(valor) && valor >= 0) {
 		this.valor = valor;
 	} else {
 		this.valor = 0;
 	}
 
+	// Obtenemos la fecha de creación del objeto al ejecutar el constructor y se guarda en Timestamp
+	this.fecha = new Date().getTime();
+
+	// Controlamos que ocurre si no se pasa ninguna etiqueta.
+	if (etiquetas.length > 0) {
+		this.etiquetas = etiquetas;
+	} else {
+		this.etiquetas = [];
+	}
+
 	// MÉTODOS ---------------------------------------
-	this.mostrarGasto = function () {
-		let texto = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+	this.mostrarGastoCompleto = function () {
+		let texto = `Gasto correspondiente a ${this.descripcion} con valor ${
+			this.valor
+		} €\nFecha: ${new Date(this.fecha).toLocaleString(
+			"es-ES"
+		)}\nEtiquetas:\n${this.listarEtiquetas()}`;
 		return texto;
 	};
 
@@ -50,9 +78,41 @@ function CrearGasto(descripcion, valor) {
 			this.valor = newValor;
 		}
 	};
+
+	// Método que genera la lista de etiquetas asociadas al gasto
+	this.listarEtiquetas = function () {
+		let texto = "";
+
+		if (this.etiquetas.length > 0) {
+			this.etiquetas.forEach(function (etiqueta) {
+				texto += `- ${etiqueta}\n`;
+			});
+		} else {
+			texto += "Sin etiquetas asignadas";
+		}
+
+		return texto;
+	};
 }
+
+let gasto1 = new CrearGasto(
+	"Compra Mercadona",
+	20.36,
+	"compra",
+	"alimentación"
+);
+console.log(gasto1.mostrarGastoCompleto());
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto };
+export {
+	mostrarPresupuesto,
+	actualizarPresupuesto,
+	listarGastos,
+	anyadirGasto,
+	borrarGasto,
+	calcularTotalGastos,
+	calcularBalance,
+	CrearGasto,
+};
