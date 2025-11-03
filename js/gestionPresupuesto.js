ab20edd2257c10bc35ee85df103fe16f54abd7be
Isabel Granados Palma <isagrapal@alu.edu.gva.es>
2025-11-03 18:38:34 +0100

    Primera práctica -Introducción a JS, Isabel Granados Palma - Código implementado sin pasar los tests

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,46 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto = 0; 
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(valor) {
     // TODO
+    if (valor >= 0) {
+        presupuesto = valor;
+    } else {
+        alert('Error, el número es negativo');
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`
 }
 
-function CrearGasto() {
+function CrearGasto(valor, descripcion) {
     // TODO
+    if (valor >= 0) {
+        this.valor = valor;
+    } else {
+        this.valor = 0;
+    }
+
+    this.descripcion = descripcion;
+
+    this.mostrarGasto = function() {
+        `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`
+    }
+
+    this.actuaizarDescripcion = function(nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    }
+
+    this.actualizarValor = function(nuevoValor) {
+        if (valor >= 0) {
+            this.valor = nuevoValor;
+        }
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
