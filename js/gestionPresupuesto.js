e97cb13e3d694d13e4822b568e8c2af03b19b8be
OscarDGimenezMunzon <oscgimmun@alu.edu.gva.es>
2025-11-05 22:13:41 +0100

    INICIO FUNDAMENTOS III. Modificación de export.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -106,6 +106,13 @@ function calcularBalance() {
     return balance;
 }
 
+function filtrarGastos() {
+
+}
+
+function agruparGastos() {
+
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -118,5 +125,7 @@ export {
     anyadirGasto,
     borrarGasto,
     calcularTotalGastos,
-    calcularBalance
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos
 }
