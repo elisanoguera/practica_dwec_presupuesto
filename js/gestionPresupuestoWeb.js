73e594111ac677c53595c898d878c2007fa3beb8
Luis Castellanos Campos <luicascam@alu.edu.gva.es>
2025-11-07 10:24:43 +0100

    Primeros cambios sobre el código tras el git pull profesora main

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,3 +1,5 @@
+import * as gestionPresupuesto from "./gestionPresupuesto.js";
+
 export function mostrarDatoEnId(idElemento, valor) {
     let elementoSeleccionado = document.getElementById(idElemento);
 
