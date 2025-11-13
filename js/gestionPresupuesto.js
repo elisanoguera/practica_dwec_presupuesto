8d0cea676888cc835bf2de27242d7fd73842aaa5
Diego Rodriguez <dierodlop@alu.edu.gva.es>
2025-11-13 19:53:10 +0100

    Segundo guardado con el objeto crear gasto modificado con los metodos y las propuedades

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,9 +1,11 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
+import eslintPluginCypress from "eslint-plugin-cypress";
+
 // TODO: Variable global
 
 let presupuesto = 0;
-let gasto;
+let gasto = [];
 let idgasto = 0;
 
 function actualizarPresupuesto(parametro) {
@@ -16,7 +18,7 @@ function actualizarPresupuesto(parametro) {
     else{
 
         console.log('Error valor no valido');
-        return -1
+        return -1;
         
     }
 
@@ -44,7 +46,6 @@ function CrearGasto(descripcion, valor) {
     //Le añadimos las propiedades
     this.descripcion = descripcion,
 
-
     //Creamos los metodos
     //metodo para mostrar el gasto
     this.mostrarGasto = function(){
@@ -64,7 +65,95 @@ function CrearGasto(descripcion, valor) {
         else{
             console.log("Valor introducido negativo. Introduzca otro valor");
         }
-    }   
+    } 
+    
+    //Añadimos los nuevos archivos a la funcion.
+    //Parametro para añadir la fecha en la que se ha registrado el gasto
+    this.fecha = Date.now();
+    //Propiedad para añadir varias etiquetas y categorizar un gasto
+    this.etiquetas = [];
+
+    //Funciones
+    this.mostrarGastoCompleto = function() {
+
+        var texto;
+
+        texto = "Gasto correspondiente a " + descripcion + " con valor " + valor + " € \n";
+        texto = texto + "Fecha: " + this.fecha.toLocaleString + "\n";
+        texto = texto + "Etiquetas: ";
+        //Si el numero de etiquecas es 0 indicamos ninguna
+        if(this.etiquetas.length === 0){
+            texto = texto + " Ninguna"
+        }
+        //recorremos todas las etiquetas y las vamos añadiendo con un salto de linea
+        else{
+            this.etiquetas.forEach(etiquetas => {
+                texto = texto + "\n - " + etiquetas; 
+            })
+        } 
+    }
+
+    this.actualizarFecha = function(nuevaFecha){
+        var lineaTiempo = Date.parse(nuevaFecha);
+        if (lineaTiempo === lineaTiempo){
+            this.fecha = lineaTiempo;
+        }
+    }
+
+    this.anyadirEtiquetas = function() {
+        for(var i = 0; i < arguments.length; i++){
+            var et = arguments[i];
+        
+            var existe = false;
+
+            for(var j = 0; j < this.etiquetas.length; j++){
+                if(this.etiquetas[j] === et){
+                existe = true;
+                break;
+                }
+            }
+        }
+
+        if(!existe) {
+            this.etiquetas.push(et)
+        }
+
+    }
+
+    this.borrarEtiquetas = function(){
+        for(var i = 0; i < arguments.length; i++){
+            var etiquetasBorrar = arguments[i];
+            var nuevaLista = [];
+
+            for(var j = 0; j < this.etiquetas.length; j++){
+                if(this.etiquetas[j] !== etiquetasBorrar){
+                    nuevaLista.push(this.etiquetas[j]);
+                }
+            }
+
+            this.etiquetas = nuevaLista;
+        }
+    }
+
+    this.borrarEtiqueta = function(){
+        var nuevasetiquetas = [];
+
+        for(var i = 0; i < this.etiquetas.length; i++){
+            var actual = this.etiquetas[i];
+            var borrar = false;
+
+            for(var j = 0; j < arguments.length; j++){
+                if(actual === arguments[j]){
+                    borrar = true;
+                    break;
+                }
+            }
+        }
+
+        if(!borrar){
+            nuevasetiquetas.push(actual);
+        }
+    }
 
 }
 
@@ -72,6 +161,7 @@ function CrearGasto(descripcion, valor) {
 
 function listarGastos() {
 
+    return gasto;
 }
 
 function anyadirGasto() {
@@ -87,7 +177,7 @@ function calcularTotalGastos() {
 }
 
 function calcularBalance() {
-    
+
 }
 
 
@@ -95,12 +185,13 @@ function calcularBalance() {
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto
     listarGastos,
     anyadirGasto,
     borrarGasto,
     calcularTotalGastos,
     calcularBalance,
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto
+    
 }
