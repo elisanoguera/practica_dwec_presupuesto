81eeb419b34cfd6ea3ee68900e0511c8fb97dbe9
Isabel Granados Palma <isagrapal@alu.edu.gva.es>
2025-11-25 20:04:03 +0100

    Fundamentos JS II - No pasa los tests

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,7 +1,9 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
-var presupuesto = 0; 
+let presupuesto = 0;
+let gastos = [];
+let idGasto = 0;
 
 function actualizarPresupuesto(valor) {
     // TODO
@@ -21,19 +23,39 @@ function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${presupuesto} €`
 }
 
-function CrearGasto(descripcion, valor) {
+function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
     // TODO
-
-    if (!isNaN(valor) && (valor >= 0)) {
-        this.valor = valor;
-    } else {
-        this.valor = 0;
-    }
 
     this.descripcion = (typeof descripcion === 'string') ? descripcion : null;
 
+    this.valor = (!isNaN(valor) && (valor >= 0)) ? valor : 0;
+
+    //Si ...etiquetas no se pasa a la función se crea un array vacío igual.
+    this.etiquetas = [...etiquetas];
+
+    if (!isNaN(Date.parse(fecha))) {
+        this.fecha = new Date().getTime();
+    } else {
+        this.fecha = Date.parse(fecha);
+    }
+
     this.mostrarGasto = function() {
         return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    }
+
+    this.mostrarGastoCompleto = function() {
+        return `Gasto correspondiente a ${descripcion} con valor ${valor} €.
+        Fecha: ${date.toString()}
+        Etiquetas:
+        ${this.listarEtiquetas()}`
+    }
+
+    this.listarEtiquetas = function() {
+        let listaEtiquetas = '';
+        for (let etiqueta of this.etiquetas) {
+            listaEtiquetas += ` - ${etiqueta}\n`;
+        }
+        return listaEtiquetas;
     }
 
     this.actualizarDescripcion = function(nuevaDescripcion) {
@@ -46,6 +68,75 @@ function CrearGasto(descripcion, valor) {
         }
     }
     
+    this.actualizarFecha = function(nuevaFecha) {
+        if (!isNaN(Date.parse(nuevaFecha))) {
+            this.fecha = nuevaFecha;
+        }
+    }
+
+    this.anyadirEtiquetas = function(...nuevasEtiquetas) {
+        for (let nuevaEtiqueta of nuevasEtiquetas) {
+            let esDuplicada = false;
+
+            for (let etiqueta of this.etiquetas) {
+                if (etiqueta == nuevaEtiqueta) {
+                    esDuplicada = true;
+                    break;
+                }
+            }
+            if (!esDuplicada) {
+                this.etiquetas.push(nuevaEtiqueta);
+            }
+        }
+    }
+
+    this.borrarEtiquetas = function(...etiquetasParaBorrar) {
+        for (let etiquetaParaBorrar of etiquetasParaBorrar) {
+            for (let etiqueta of this.etiquetas) {
+                if (etiqueta == etiquetaParaBorrar) {
+                    indiceBorrar = this.etiquetas.indexOf(etiqueta);
+                    this.etiquetas.splice(indiceBorrar, 1);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function listarGastos() {
+    return gastos;
+}
+
+function anyadirGasto(id, gasto) {
+    this.id = idGasto;
+    idGasto++;
+    gastos.push(gasto); 
+}
+
+function borrarGasto(id) {
+    let indexGastoEliminar = undefined;
+    for (let gasto of gastos) {
+        if (gasto.id === id) {
+        indexGastoEliminar = gastos.indexOf(gasto);
+            break;
+        }
+    }
+    
+    if (indexGastoEliminar !== undefined) {
+        gastos.splice(indexGastoEliminar, 1);
+    }
+}
+
+function calcularTotalGastos() {
+    let totalGastos = 0;
+    for (let gasto of gastos) {
+        totalGastos += gasto.value;
+    }
+    return totalGastos;
+}
+
+function calcularBalance() {
+    return (presupuesto - calcularTotalGastos());
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
