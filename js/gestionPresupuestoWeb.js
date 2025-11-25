9b840aca00e347a366ca795face41510fe282fb5
RAQUEL2LILLO2425 <lillomarcoraquel@gmail.com>
2025-11-25 17:00:46 +0100

    probando

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -16,7 +16,7 @@ function mostrarDatoEnId(idElemento, valor) {
   }
   
 
-
+//probando
 
 // esta funcion recibe el id del elemento contenedor y un objeto gasto (recordemos que gasto tiene: descripcion, fecha, valor, etiquetas)
 function mostrarGastoWeb(idElemento, gasto) {
