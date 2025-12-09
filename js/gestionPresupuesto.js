a5149974fd7d47417d1959790c93b5ecd75ccc53
JAVIER VAZQUEZ GOMEZ <javvazgom@alu.edu.gva.es>
2025-12-09 22:44:30 +0100

    Objeto gasto: construcción y método mostrargasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,23 +4,43 @@
 let presupuesto = 0;
 
 
+// Función actualizarPresupuesto
 function actualizarPresupuesto(nuevoPresupuesto) {
     if ((typeof nuevoPresupuesto === 'number') && (nuevoPresupuesto >= 0)) {
         presupuesto = nuevoPresupuesto;
         return presupuesto;
     } else {
         console.error('El presupuesto debe ser un número positivo.');
-        alert('El presupuesto debe ser un número positivo.');
+        //alert('El presupuesto debe ser un número positivo.'); //Los alerts dan error con los tests
         return -1;
     }
 }
 
+// Función mostrarPresupuesto
 function mostrarPresupuesto() {
-    // TODO
+    let mensaje = `Tu presupuesto actual es de ${presupuesto} €`;
+    console.log(mensaje);
+    return mensaje;
 }
 
-function CrearGasto() {
-    // TODO
+// Función CrearGasto
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion;
+
+    // Validación del valor
+    if ((typeof valor === 'number') && (valor >= 0)) {
+        this.valor = valor;
+    } else {
+        console.error('El valor del gasto debe ser un número positivo.');
+        this.valor = 0;
+    }
+
+    // Método mostrarGasto
+    this.mostrarGasto = function() {
+        let mensaje = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+        console.log(mensaje);
+        return mensaje;
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
